e09b1801c019edccec8564aac0746bb1c3b37480
Adam S <asa188@sfu.ca>
2015-08-11 16:47:03 -0700

    Adding display of working service provider fact and function

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,5 @@
 class sfu_stdlib {
+  notify { "service_provider: ${::service_provider}": }
+  $default_service_provider = default_service_provider_merge()
+  notify { "default_service_provider: ${default_service_provider}": }
 }
-include sfu_stdlib
